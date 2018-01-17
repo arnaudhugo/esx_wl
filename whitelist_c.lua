81874a653066ca550ec11feec1ac7b877c32d0ff
arnaudhugo <33054060+arnaudhugo@users.noreply.github.com>
2018-01-17 22:09:01 +0100

    1 commit

---
+++ whitelist_c.lua
@@ -0,0 +1,7 @@
+local firstSpawn = false
+AddEventHandler("playerSpawned", function()
+	if(not firstSpawn) then
+		TriggerServerEvent("rocade:removePlayerToInConnect")
+		firstSpawn = true
+	end
+end)
